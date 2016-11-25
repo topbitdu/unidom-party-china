1ed4d6dd9e797a9045dd8184610397ab8a412593
Topbit Du <topbit.du@gmail.com>
2016-11-25 14:25:10 +0800

    1, Improve the Marital Status enum code for the coding style.

--- app/types/unidom/party/china/marital_status.rb
+++ app/types/unidom/party/china/marital_status.rb
@@ -1,6 +1,6 @@
 # Marital Status 是中国的婚姻状况代码。符合 GB/T 2261.2-2003 标准。之前的标准是 GB/T 4766-1984 。
 
-class Unidom::Party::China::MaritalStatus  < ActiveRecord::Type::Value
+class Unidom::Party::China::MaritalStatus < ActiveRecord::Type::Value
 
   include ProgneTapera::EnumConfig
 
