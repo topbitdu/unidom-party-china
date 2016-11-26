d083d401322caf411ec4d0d88a370d4ec3799612
Topbit Du <topbit.du@gmail.com>
2016-11-26 18:04:54 +0800

    1, Political Affiliation enum code.

---
+++ app/types/unidom/party/china/political_affiliation.rb
@@ -0,0 +1,9 @@
+# Political Affiliation 是中国的政治面貌代码。符合 GB/T 4762-1984 标准。
+
+class Unidom::Party::China::PoliticalAffiliation < ActiveRecord::Type::Value
+
+  include ProgneTapera::EnumConfig
+
+  enum :unidom_china_political_affiliation
+
+end
