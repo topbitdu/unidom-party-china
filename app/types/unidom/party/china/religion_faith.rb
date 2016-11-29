d3d67abc1ee34bca5ecced2342ffc59a44090dac
Topbit Du <topbit.du@gmail.com>
2016-11-29 09:40:59 +0800

    1, Religion Faith enum code.

---
+++ app/types/unidom/party/china/religion_faith.rb
@@ -0,0 +1,9 @@
+# Religion Faith 是中国的宗教信仰代码。符合 GA 214.12-2004 标准。
+
+class Unidom::Party::China::ReligionFaith < ActiveRecord::Type::Value
+
+  include ProgneTapera::EnumConfig
+
+  enum :unidom_china_religion_faith
+
+end
