2811f0677f9197c06e18417847a06a695bbd63bb
Topbit Du <topbit.du@gmail.com>
2016-12-07 00:10:15 +0800

    1, Formal Schooling enum code.

---
+++ app/types/unidom/party/china/formal_schooling.rb
@@ -0,0 +1,9 @@
+# Formal Schooling 是中国的学历。符合 GB/T 4658-2006 标准。
+
+class Unidom::Party::China::FormalSchooling < ActiveRecord::Type::Value
+
+  include ProgneTapera::EnumConfig
+
+  enum :unidom_china_formal_schooling
+
+end
