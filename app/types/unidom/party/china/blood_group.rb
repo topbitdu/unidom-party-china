b8fe07f502b881b8d22f2c136eeb247651b4e67d
Topbit Du <topbit.du@gmail.com>
2016-12-08 07:02:31 +0800

    1, Blood Group enum code.

---
+++ app/types/unidom/party/china/blood_group.rb
@@ -0,0 +1,9 @@
+# Blood Group 是中国的血型。符合 GA/T 2000-2016 标准。
+
+class Unidom::Party::China::BloodGroup < ActiveRecord::Type::Value
+
+  include ProgneTapera::EnumConfig
+
+  enum :unidom_china_blood_group
+
+end
