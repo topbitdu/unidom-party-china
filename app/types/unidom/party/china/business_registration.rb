4210a64ba847bd24f59963b04dca251666945bfc
Topbit Du <topbit.du@gmail.com>
2017-01-07 14:37:32 +0800

    1, Improve the Business Registration enum code for the document.

--- app/types/unidom/party/china/business_registration.rb
+++ app/types/unidom/party/china/business_registration.rb
@@ -1,3 +1,4 @@
+##
 # Business Registration 是中国的工商注册。
 # 关于划分企业登记注册类型的规定 http://www.stats.gov.cn/tjsj/tjbz/200610/t20061018_8657.html
 # 个体经营分类与代码 http://www.stats.gov.cn/tjsj/tjbz/200611/t20061123_8663.html
