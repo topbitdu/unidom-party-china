25660a9673635c82644f2880f678bf117c799b6e
Topbit Du <topbit.du@gmail.com>
2017-02-02 19:20:47 +0800

    1, Types RSpec examples manifest.

---
+++ lib/unidom/party/china/types_rspec.rb
@@ -0,0 +1,8 @@
+require 'rspec/types/unidom/party/china/blood_group_spec'
+require 'rspec/types/unidom/party/china/business_registration_spec'
+require 'rspec/types/unidom/party/china/ethnicity_alphabetic_spec'
+require 'rspec/types/unidom/party/china/ethnicity_numeric_spec'
+require 'rspec/types/unidom/party/china/formal_schooling_spec'
+require 'rspec/types/unidom/party/china/marital_status_spec'
+require 'rspec/types/unidom/party/china/political_affiliation_spec'
+require 'rspec/types/unidom/party/china/religion_faith_spec'
