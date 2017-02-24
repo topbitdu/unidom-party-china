b975795a7840070ca4a5f4a4d709472da413ccf5
Topbit Du <topbit.du@gmail.com>
2017-02-24 09:32:08 +0800

    1, Improve the Ethnicity Alphabetic spec to follow GB 3304-1991.

--- lib/rspec/types/unidom/party/china/ethnicity_alphabetic_spec.rb
+++ lib/rspec/types/unidom/party/china/ethnicity_alphabetic_spec.rb
@@ -6,11 +6,10 @@ describe Unidom::Party::China::Ethnicity::Alphabetic, type: :type do
   after :each do
   end
 
-  it_behaves_like 'ProgneTapera::EnumConfig', 57, [
+  it_behaves_like 'ProgneTapera::EnumConfig', 56, [
     { code: 'HA', name: 'han',     localized_name: '汉族'   },
     { code: 'MG', name: 'mongol',  localized_name: '蒙古族' },
     { code: 'ZA', name: 'tibetan', localized_name: '藏族'   },
-    { code: 'CS', name: 'korean',  localized_name: '朝鲜族' },
-    { code: 'ZZ', name: 'other',   localized_name: '其他'   } ]
+    { code: 'CS', name: 'korean',  localized_name: '朝鲜族' } ]
 
 end
